8e8230cb9bd8724d87714e27a35ec0373b1ab33e
sansuern.dev <sansuern.dev@myorder.ai>
2025-02-20 21:41:41 +0700

    servier

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -1,9 +1,6 @@
-const appBaseUrl = "http://localhost:8080";
-const appBaseApiUrl = "http://localhost:8080/api";
-
+const appBaseUrl = "http://119.59.99.43:8080";
+const appBaseApiUrl = "http://119.59.99.43:8080/api";
 const branchName = "App Easy Taokae - 7-11 ซอยโรจน์เสรี (ทางรถไฟสายเก่า) รหัสร้าน 19646";
-
-
 
 const marketProductListState = "MarketProductListState";
 const marketCustomerListState = "MarketCustomerListState";
